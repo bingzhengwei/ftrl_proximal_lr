fac71e2fe0ea6cbbc47281ba0b323149601384bd
bingzhengwei <weibingzheng@gmail.com>
2014-12-03 23:44:19 +0800

    print accuracy when predict

--- ftrl_predict.cpp
+++ ftrl_predict.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <cstdlib>
 #include "common.h"
+#include "util.h"
 #include "ftrl_solver.h"
 
 void print_usage(int argc, char* argv[]) {
@@ -46,6 +47,8 @@ int main(int argc, char* argv[]) {
 	std::vector<std::pair<size_t, double> > x;
 	FILE* fp = fopen(test_file.c_str(), "r");
 	FILE* wfp = fopen(output_file.c_str(), "w");
+	size_t cnt = 0, correct = 0;
+	double loss = 0.;
 	while(read_line(fp, line, max_line_len) != NULL) {
 		if (!parse_line(line, y, x)) {
 			fprintf(wfp, "0.0\n");
@@ -54,6 +57,19 @@ int main(int argc, char* argv[]) {
 		double pred = model.Predict(x);
 		pred = std::max(std::min(pred, 1. - 10e-15), 10e-15);
 		fprintf(wfp, "%lf\n", pred);
+
+		++cnt;
+		double pred_label = 0;
+		if (pred > 0.5) pred_label = 1;
+		if (double_equal(pred_label, y)) ++correct;
+
+		pred = std::max(std::min(pred, 1. - 10e-15), 10e-15);
+		loss += y > 0 ? -log(pred) : -log(1. - pred);
+	}
+
+	if (cnt > 0) {
+		printf("Accuracy = %.2lf%% (%lu/%lu)\n", (double) correct / cnt * 100, correct, cnt);
+		printf("Log-likelihood = %lf\n", loss / cnt);
 	}
 
 	free(line);
